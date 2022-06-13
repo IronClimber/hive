2edb94b3ab6bf08e1f64d4c27835e3d7f30471fc
Stepan Kostusiev <ironclimber@gmail.com>
2022-06-13 15:31:08 +0300

    add two sensors, remove motion

--- arduino/HiveMonitoring/HiveMonitoring.ino
+++ arduino/HiveMonitoring/HiveMonitoring.ino
@@ -5,7 +5,6 @@
 #define SENSOR_ERROR_VALUE (-404.0)
 
 #define REQUEST_PERIOD (5000)
-#define MOTION_PERIOD (100)
 
 #define DURATION_LIMIT (60)
 #define PERIOD_LIMIT (120)
@@ -16,16 +15,6 @@
 
 //PINOUT
 #define RELAY_PIN 7
-
-#define MOTION_SENSOR_1_PIN 8
-#define MOTION_SENSOR_2_PIN 9
-#define MOTION_SENSOR_3_PIN 10
-#define MOTION_SENSOR_4_PIN 11
-
-int State1 = LOW;
-int State2 = LOW;
-int State3 = LOW;
-int State4 = LOW;
 
 //------Request variable---------------------
 char PostResponse[300];
@@ -38,12 +27,6 @@ int counter = 0;
 int fan = 0;
 int period = 2;
 int duration = 1;
-
-//Motion sensors
-int motion1 = 0;
-int motion2 = 0;
-int motion3 = 0;
-int motion4 = 0;
 
 //Sensor 1
 float humidity1 = 0;
@@ -68,6 +51,14 @@ float tempc5 = 0;
 //Sensor 6
 float humidity6 = 0;
 float tempc6 = 0;
+
+//Sensor 7
+float humidity7 = 0;
+float tempc7 = 0;
+
+//Sensor 8
+float humidity8 = 0;
+float tempc8 = 0;
 
 //Temperature and humidity sensors
 #define DHTPIN1 1
@@ -94,16 +85,17 @@ DHT dht5(DHTPIN5, DHTTYPE5);
 #define DHTTYPE6 DHT22   
 DHT dht6(DHTPIN6, DHTTYPE6);
 
-//Timers
-unsigned long tmr1 = 0;
-unsigned long tmr2 = 0;
-unsigned long tmr3 = 0;
-unsigned long tmr4 = 0;
+#define DHTPIN7 8
+#define DHTTYPE7 DHT22   
+DHT dht7(DHTPIN7, DHTTYPE7);
+
+#define DHTPIN8 9
+#define DHTTYPE8 DHT22   
+DHT dht8(DHTPIN8, DHTTYPE8);
 
 //Timer to control fan
 uint32_t fan_timer = millis();
 uint32_t last_request = millis();
-uint32_t last_motion = millis();
 
 //-------WiFi variables ---------
 
@@ -154,12 +146,6 @@ void setup() {
   //Fan output
   pinMode (RELAY_PIN, OUTPUT);
 
-  //Motion sensors
-  pinMode (8, INPUT); 
-  pinMode (9, INPUT); 
-  pinMode (10, INPUT);  
-  pinMode (11, INPUT);
-
   //Init DHT sensors
   dht1.begin();
   dht2.begin();
@@ -167,62 +153,13 @@ void setup() {
   dht4.begin();
   dht5.begin();
   dht6.begin();
-
+  dht7.begin();
+  dht8.begin();
 }
 
 //---------LOOP---------------------------------------
 
 void loop() {
-
-//----------------------Read motion sensors -----------
-  if (millis()-last_motion > MOTION_PERIOD) {
-    last_motion = millis();
-
-    //Read motion sensors
-    int motionState1 = digitalRead(MOTION_SENSOR_1_PIN);
-    if ( motionState1 == LOW) {
-      if (State1 == LOW) {
-        motion1++;
-        State1 = HIGH;
-      }
-    }
-    else {
-      State1 = LOW;
-    }
-
-    int motionState2 = digitalRead(MOTION_SENSOR_2_PIN);
-    if ( motionState2 == LOW) {
-      if (State2 == LOW) {
-        motion2++;
-        State2 = HIGH;
-      }
-    }
-    else {
-      State2 = LOW;
-    }
-
-    int motionState3 = digitalRead(MOTION_SENSOR_3_PIN);
-    if ( motionState3 == LOW) {
-      if (State3 == LOW) {
-        motion3++;
-        State3 = HIGH;
-      }
-    }
-    else {
-      State3 = LOW;
-    }
-
-    int motionState4 = digitalRead(MOTION_SENSOR_4_PIN);
-    if ( motionState4 == LOW) {
-      if (State4 == LOW) {
-        motion4++;
-        State4 = HIGH;
-      }
-    }
-    else {
-      State4 = LOW;
-    }
-  }
 
 //---------------------------------------
   if (millis()-last_request > REQUEST_PERIOD) {
@@ -340,13 +277,27 @@ void loop() {
     if (isnan(humidity6)) humidity6 = SENSOR_ERROR_VALUE;
     float tempc6 = dht6.readTemperature();
     if (isnan(tempc6)) tempc6 = SENSOR_ERROR_VALUE;
+
+    //Sensor 7
+    float humidity7 = dht7.readHumidity();
+    if (isnan(humidity7)) humidity7 = SENSOR_ERROR_VALUE;
+    float tempc7 = dht7.readTemperature();
+    if (isnan(tempc7)) tempc7 = SENSOR_ERROR_VALUE;
+
+    //Sensor 8
+    float humidity8 = dht8.readHumidity();
+    if (isnan(humidity8)) humidity8 = SENSOR_ERROR_VALUE;
+    float tempc8 = dht8.readTemperature();
+    if (isnan(tempc8)) tempc8 = SENSOR_ERROR_VALUE;
+
     Serial.println(humidity1);
     Serial.println(humidity2);
     Serial.println(humidity3);
     Serial.println(humidity4);
     Serial.println(humidity5);
     Serial.println(humidity6);
-
+    Serial.println(humidity7);
+    Serial.println(humidity8);
 
     //-----------------------Communication part------------------------
     client.stop();
@@ -361,22 +312,22 @@ void loop() {
             "%s%s\"audio2\"\r\n\r\n%d"
             "%s%s\"audio3\"\r\n\r\n%d"
             "%s%s\"audio4\"\r\n\r\n%d"
-            "%s%s\"motion1\"\r\n\r\n%d"
-            "%s%s\"motion2\"\r\n\r\n%d"
-            "%s%s\"motion3\"\r\n\r\n%d"
-            "%s%s\"motion4\"\r\n\r\n%d"
             "%s%s\"humidity1\"\r\n\r\n%d.%01d\r\n"
             "%s%s\"humidity2\"\r\n\r\n%d.%01d\r\n"
             "%s%s\"humidity3\"\r\n\r\n%d.%01d\r\n"
             "%s%s\"humidity4\"\r\n\r\n%d.%01d\r\n"
             "%s%s\"humidity5\"\r\n\r\n%d.%01d\r\n"
             "%s%s\"humidity6\"\r\n\r\n%d.%01d\r\n"
+            "%s%s\"humidity7\"\r\n\r\n%d.%01d\r\n"
+            "%s%s\"humidity8\"\r\n\r\n%d.%01d\r\n"
             "%s%s\"tempc1\"\r\n\r\n%d.%01d\r\n"
             "%s%s\"tempc2\"\r\n\r\n%d.%01d\r\n"
             "%s%s\"tempc3\"\r\n\r\n%d.%01d\r\n"
             "%s%s\"tempc4\"\r\n\r\n%d.%01d\r\n"
             "%s%s\"tempc5\"\r\n\r\n%d.%01d\r\n"
             "%s%s\"tempc6\"\r\n\r\n%d.%01d\r\n"
+            "%s%s\"tempc7\"\r\n\r\n%d.%01d\r\n"
+            "%s%s\"tempc8\"\r\n\r\n%d.%01d\r\n"
             "--%s--",
             __DELIMITER, __FORM_DATA, fan,
             __DELIMITER, __FORM_DATA, period, 
@@ -385,22 +336,22 @@ void loop() {
             __DELIMITER, __FORM_DATA, analogRead(2),
             __DELIMITER, __FORM_DATA, analogRead(3),
             __DELIMITER, __FORM_DATA, analogRead(4),
-            __DELIMITER, __FORM_DATA, motion1,
-            __DELIMITER, __FORM_DATA, motion2,
-            __DELIMITER, __FORM_DATA, motion3,
-            __DELIMITER, __FORM_DATA, motion4,
             __DELIMITER, __FORM_DATA, int(humidity1), (int)(humidity1*100)%100,
             __DELIMITER, __FORM_DATA, int(humidity2), (int)(humidity2*100)%100, 
             __DELIMITER, __FORM_DATA, int(humidity3), (int)(humidity3*100)%100, 
             __DELIMITER, __FORM_DATA, int(humidity4), (int)(humidity4*100)%100, 
             __DELIMITER, __FORM_DATA, int(humidity5), (int)(humidity5*100)%100, 
             __DELIMITER, __FORM_DATA, int(humidity6), (int)(humidity6*100)%100,
+            __DELIMITER, __FORM_DATA, int(humidity7), (int)(humidity7*100)%100, 
+            __DELIMITER, __FORM_DATA, int(humidity8), (int)(humidity8*100)%100,
             __DELIMITER, __FORM_DATA, int(tempc1), (int)(tempc1*100)%100,
             __DELIMITER, __FORM_DATA, int(tempc2), (int)(tempc2*100)%100, 
             __DELIMITER, __FORM_DATA, int(tempc3), (int)(tempc3*100)%100, 
             __DELIMITER, __FORM_DATA, int(tempc4), (int)(tempc4*100)%100, 
             __DELIMITER, __FORM_DATA, int(tempc5), (int)(tempc5*100)%100, 
             __DELIMITER, __FORM_DATA, int(tempc6), (int)(tempc6*100)%100,
+            __DELIMITER, __FORM_DATA, int(tempc7), (int)(tempc7*100)%100, 
+            __DELIMITER, __FORM_DATA, int(tempc8), (int)(tempc8*100)%100,
             __BOUNDARY
     );
     
